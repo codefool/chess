8a02c4d2fe79257f5a8c530f1e90e8434be833f6
Garyl Hester <ghester@codefool.org>
2022-05-08 18:49:32 -0500

    Checkpoint

--- dht/dht.h
+++ dht/dht.h
@@ -26,19 +26,25 @@ extern const char *p_naught;
 
 size_t get_thread_hash();
 
-// cdfl::dht<PositionHash, PositionPacked, PositionRec> tbl;
+// cdfl::dht<PositionRec, PositionRecHasher<PositionRec>> tbl;
+// tbl.search(pr, tbl::Key key); - how to get the hash back?
 template<class T>
 struct md5
 {
     static const size_t hash_size = 32;
     typedef std::string hash_t;
 
-    static void hash(T key, hash_t& buff)
+    static void hash(T data, hash_t& buff)
     {
         MD5 md5;
-        md5.update((ucharptr_c)&key, sizeof(T));
+        md5.update((ucharptr_c)&data, sizeof(T));
         md5.finalize();
         buff = md5.hexdigest();
+    }
+
+    static bool compare( T lhs, T rhs )
+    {
+        return std::memcmp(&lhs, &rhs) == 0;
     }
 };
 
@@ -48,7 +54,9 @@ struct BucketFile
     std::FILE  *_fp;
     std::string _fspec;
 
-    BucketFile() {}
+    BucketFile()
+    : _fp(nullptr)
+    {}
 
     BucketFile(std::FILE *fp, std::string fspec)
     : _fp(fp), _fspec(fspec)
@@ -57,43 +65,45 @@ struct BucketFile
 
 typedef std::shared_ptr<BucketFile> BucketFilePtr;
 
-template<class _Key, class _Val, class _Hasher = md5<_Key>>
+template<class _Data, class _Hasher = md5<_Data>>
 class dht
 {
 public:
-    typedef _Key    key_t;
-    typedef _Val    val_t;
+    typedef _Data   data_t;
     typedef _Hasher hasher_t;
 
     typedef typename hasher_t::hash_t hash_t;
 
-private:
 #pragma pack(1)
-    struct _k
+    struct Key
     {
         hash_t     _h;
         coll_cnt_t _c;
 
-        _k(key_t k)
-        : _c(UNKNOWN)
+        Key() {}
+        Key( data_t d )
+        : _c( UNKNOWN )
         {
-            hasher_t::hash(k, _h);
+            reset(d);
+        }
+        void reset(data_t d)
+        {
+            hasher_t::hash( d, _h );
         }
     };
 
-    struct _r
+    struct Rec
     {
-        key_t   _k;
-        val_t   _v;
+        Key     _k;
+        data_t  _d;
     };
 #pragma pack()
 
 public:
-    const size_t key_disk_len = sizeof( _k );
-    const size_t rec_len      = sizeof( _r );
     const size_t hash_len     = hasher_t::hash_size;
-    const size_t key_len      = sizeof( key_t );
-    const size_t val_len      = sizeof( val_t );
+    const size_t data_len     = sizeof( data_t );
+    const size_t key_len      = sizeof( Key );
+    const size_t rec_len      = sizeof( Rec );
 
     std::string                 base_path;
     std::string                 base_name;
@@ -112,16 +122,31 @@ public:
         std::filesystem::create_directories( base_path );
     }
 
-    hash_t hash( key_t k )
+    ~dht()
     {
-        _k key( k );
-        return key._h;
+        for ( auto i : buck_map )
+            if (i.second->_fp != nullptr)
+            {
+                fclose(i.second->_fp);
+            }
+    }
+
+    hash_t hash( data_t d )
+    {
+        hash_t hash;
+        hasher_t::hash(d, hash);
+        return hash;
+    }
+
+    Key make_key(data_t d)
+    {
+        return Key(d);
     }
 
     std::string get_buck_id(uint64_t hash)
     {
-        uchar buff[hash_len+1];
-        std::sprintf(buff, "%0*lx", hash_len, hash );
+        char buff[hash_len+1];
+        std::sprintf(buff, "%0*lx", (int)hash_len, hash );
         return get_buck_id(std::string(buff));
     }
 
@@ -145,7 +170,7 @@ public:
         if ( fp == nullptr )
         {
             std::cout << "Error opening bucket file " << fspec << ' ' << errno << " - terminating" << std::endl;
-            exit(1);
+            return nullptr;
         }
         opencnt++;
         buck_map[ bucket_id ] = std::make_shared<BucketFile>(fp, fspec);
@@ -163,9 +188,57 @@ public:
         ss << path << '/' << name << '_' << bucket_id;
         return ss.str();
     }
+
+    BuffPtr get_file_buff()
+    {
+        size_t id_hash = get_thread_hash();
+        if ( buff_map.find(id_hash) == buff_map.end() )
+        {
+            buff_map[id_hash] = BuffPtr(
+                new unsigned char[TABLE_BUFF_SIZE],
+                std::default_delete<uchar[]>()
+            );
+        }
+        return buff_map[id_hash];
+    }
+
+    off_t search(data_t data)
+    {
+        Key k;
+        return search(data, k);
+    }
+
+    off_t search(data_t data, Key& key)
+    {
+        key.reset( data );
+        std::string buck_id = get_buck_id( key._h );
+        BucketFilePtr bf = get_bucket( buck_id );
+        int max_item_cnt = TABLE_BUFF_SIZE / rec_len;
+        std::lock_guard<std::mutex> lock( bf->_m );
+        BuffPtr buff = get_file_buff();
+        std::fseek( bf->_fp, 0, SEEK_SET );
+        fpos_t pos;  // file position at start of block
+        std::fgetpos( bf->_fp, &pos );
+        size_t rec_cnt = std::fread( buff.get(), rec_len, max_item_cnt, bf->_fp );
+        while ( rec_cnt > 0 )
+        {
+            unsigned char *p = buff.get();
+            for ( int i(0); i < rec_cnt; ++i )
+            {
+                if ( !std::memcmp( p, &key, key_len ) )
+                {
+                    std::memcpy( &data, p, rec_len );
+                    off_t off = pos.__pos + ( p - buff.get() );
+                    return off;
+                }
+                p += rec_len;
+            }
+            std::fgetpos( bf->_fp, &pos );
+            rec_cnt = std::fread( buff.get(), rec_len, max_item_cnt, bf->_fp );
+        }
+        return -1;
+    }
 };
-
-
 
 
 } // namespace cdfl
--- dht/dht_test.cpp
+++ dht/dht_test.cpp
@@ -3,26 +3,80 @@
 #include "dht.h"
 
 typedef uint64_t HashType;
-struct Key
-{
-    char a[128];
-};
 
-struct Val
+#pragma pack(1)
+struct Data
 {
+    struct
+    {
+        uint32_t gi;
+        uint64_t pop;
+        uint64_t hi;
+        uint64_t lo;
+    } k;
     char b[128];
 };
+#pragma pack()
 
+// template<class T>
+// struct zobrist_hasher
+// {
+//     static const size_t hash_size = 32;
+//     typedef std::string hash_t;
 
-cdfl::dht<Key, Val> aaa("/home/codefool/work/test", "bogus");
+//     static void hash(T data, hash_t& buff)
+//     {
+//         MD5 md5;
+//         md5.update((cdfl::ucharptr_c)&data.k, sizeof(data.k));
+//         md5.finalize();
+//         buff = md5.hexdigest();
+//     }
+//     static bool compare( T lhs, T rhs )
+//     {
+//         return std::memcmp(&lhs, &rhs, sizeof(Data::k)) == 0;
+//     }
 
+// };
+
+struct zobrist_hasher
+{
+    static const size_t hash_size = 32;
+    typedef std::string hash_t;
+
+    static void hash(Data data, hash_t& buff)
+    {
+        MD5 md5;
+        md5.update((cdfl::ucharptr_c)&data.k, sizeof(data.k));
+        md5.finalize();
+        buff = md5.hexdigest();
+    }
+    static bool compare( Data lhs, Data rhs )
+    {
+        return std::memcmp(&lhs, &rhs, sizeof(Data::k)) == 0;
+    }
+};
+
+cdfl::dht<Data, zobrist_hasher> aaa("/home/codefool/work/test", "bogus");
+typedef cdfl::dht<Data, zobrist_hasher>::Key aaaKey;
 
 int main(int argc, char **argv)
 {
-    Key k;
-    auto hash = aaa.hash(k);
+    Data d;
+    d.k.gi  = 0x12345678;
+    d.k.pop = 0x0123456789ABCDEFULL;
+    d.k.hi  = 0x0123456789ABCDEFULL;
+    d.k.lo  = 0x0123456789ABCDEFULL;
+
+    Data e = d;
+    e.k.lo  = 0x0123456789ABCDEEULL;
+
+    std::cout << zobrist_hasher::compare(d,e) << std::endl;
+
+    auto hash = aaa.hash(d);
     std::string id = aaa.get_buck_id(hash);
     std::cout << hash << ' ' << id << ' ' << aaa.get_bucket_fspec(id) << ' ' << (int)cdfl::MAX_COLL << std::endl;
     aaa.get_bucket(id);
+    aaaKey k;
+    aaa.search(d, k);
     return 0;
 }
